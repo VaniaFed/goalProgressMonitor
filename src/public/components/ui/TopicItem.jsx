23d3ea2cf073f3d0681aa25d9b0ba91c542c46bd
VaniaFed <ztankist@gmail.com>
2019-06-05 19:00:24 +0300

    done change progress by button

--- src/public/components/ui/TopicItem.jsx
+++ src/public/components/ui/TopicItem.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import Control from './Control/Control'
+import { calcPercent } from '../../libs/functions'
 import css from './topicItem.module.sass'
 
 export default class TopicItem extends Component {
@@ -7,20 +8,18 @@ export default class TopicItem extends Component {
     return this.props.topic.current !== nextProps.topic.current
   }
 
-  componentWillUpdate () {
-    console.log(this.props.topic)
-  }
-
   render () {
     const { topic, onIncrease, onDecrease } = this.props
+    const currentPercent = calcPercent(topic)
+
     return (
       <div className={css.item}>
         <h5 className={css.title}>{topic.title}</h5>
         <Control currentValue={topic.current} onIncrease={onIncrease} onDecrease={onDecrease} />
         <div className={css.progressContainer}>
           <div className="progress">
-            <div className="progress-bar" role="progressbar" style={{ width: '25%' }} aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-            <h5 className={css.percent}>25%</h5>
+            <div className="progress-bar" role="progressbar" style={{ width: currentPercent + '%' }} aria-valuenow={currentPercent} aria-valuemin="0" aria-valuemax="100"></div>
+            <h5 className={css.percent}>{currentPercent}%</h5>
           </div>
         </div>
       </div>
