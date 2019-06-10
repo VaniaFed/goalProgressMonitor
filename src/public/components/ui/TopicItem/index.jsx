bc083565563446c33851c6ae21e8c48c1e3ccfb2
VaniaFed <ztankist@gmail.com>
2019-06-10 12:17:52 +0300

    I again make hide and show control and fixed percent's formula

--- src/public/components/ui/TopicItem/index.jsx
+++ src/public/components/ui/TopicItem/index.jsx
@@ -8,21 +8,21 @@ import css from './topicItem.module.sass';
 
 @observer
 class TopicItem extends Component {
-  // constructor(props) {
-  //   super(props);
-  //   // this.state = { displayControlTopic: false };
+  constructor(props) {
+    super(props);
+    this.state = { displayControlTopic: false };
 
-  //   // this.showControl = this.showControl.bind(this);
-  //   // this.hideControl = this.hideControl.bind(this);
-  // }
+    this.showControl = this.showControl.bind(this);
+    this.hideControl = this.hideControl.bind(this);
+  }
 
-  // showControl() {
-  //   this.setState({ displayControlTopic: true });
-  // }
+  showControl() {
+    this.setState({ displayControlTopic: true });
+  }
 
-  // hideControl() {
-  //   this.setState({ displayControlTopic: false });
-  // }
+  hideControl() {
+    this.setState({ displayControlTopic: false });
+  }
 
   render() {
     const { topic, onIncrease, onDecrease } = this.props;
@@ -30,8 +30,8 @@ class TopicItem extends Component {
     return (
       <div
         className={css.item}
-        // onMouseOver={this.showControl}
-        // onMouseLeave={this.hideControl}
+        onMouseOver={this.showControl}
+        onMouseLeave={this.hideControl}
       >
         <h5 className={css.title}>{topic.title}</h5>
         <ProgressBar
@@ -40,21 +40,22 @@ class TopicItem extends Component {
           currentPercent={currentPercent}
         />
         <div className={css.controlTopic}>
-          {/* <div className={`${css.controlTopic} ${!this.state.displayControlTopic && 'hidden'}`}> */}
-          <button
-            type="button"
-            className={`${css.buttonControl} buttonDefault`}
-            onClick={onIncrease}
-          >
-          +
-          </button>
-          <button
-            type="button"
-            className={`${css.buttonControl} buttonDefault`}
-            onClick={onDecrease}
-          >
-          -
-          </button>
+          <div className={`${css.controlTopic} ${!this.state.displayControlTopic && 'hidden'}`}>
+            <button
+              type="button"
+              className={`${css.buttonControl} buttonDefault`}
+              onClick={onIncrease}
+            >
+            +
+            </button>
+            <button
+              type="button"
+              className={`${css.buttonControl} buttonDefault`}
+              onClick={onDecrease}
+            >
+            -
+            </button>
+          </div>
         </div>
       </div>
     );
--- src/public/libs/functions.js
+++ src/public/libs/functions.js
@@ -3,9 +3,9 @@ const isInteger = num => {
 }
 export const calcPercent = topic => {
   const { start, finish, current } = topic
-  if (current === finish || current > finish) {
+  if (current >= finish) {
     return 100;
   }
-  const percent = (100 / (finish - start) * current) - start
-  return (isInteger(percent)) ? percent : percent.toFixed(2);
+  const percent = Number(100 / (finish - start) * (current - start));
+  return percent.toFixed(2);
 };
